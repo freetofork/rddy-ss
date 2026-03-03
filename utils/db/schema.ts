494a6e708dbe6bde661a1727d086a7ac3feea288
Free To Fork <zeroeum@yahoo.com>
2026-03-03 09:41:16 -0300

    Update schema.ts

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { boolean, pgTable, text } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users_table', {
     id: text('id').primaryKey(),
@@ -6,6 +6,14 @@ export const usersTable = pgTable('users_table', {
     email: text('email').notNull().unique(),
     plan: text('plan').notNull(),
     stripe_id: text('stripe_id').notNull(),
+});
+
+// Add this table
+export const licenseKeysTable = pgTable('license_keys', {
+    id: text('id').primaryKey(),
+    key: text('key').notNull().unique(),
+    email: text('email').notNull(),
+    is_used: boolean('is_used').default(false),
 });
 
 export type InsertUser = typeof usersTable.$inferInsert;
