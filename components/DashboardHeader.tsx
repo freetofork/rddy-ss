b21dccf3c4eb44c00d522d760af7adc24cfe4bf8
Free To Fork <zeroeum@yahoo.com>
2026-03-07 10:55:46 -0300

    Update DashboardHeader.tsx

--- components/DashboardHeader.tsx
+++ components/DashboardHeader.tsx
@@ -21,24 +21,15 @@ export default async function DashboardHeader() {
         <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-14 max-w-screen-2xl items-center">
                 <div className="mr-4 hidden md:flex">
-                    <Link className="mr-2 flex items-center space-x-2" href="">
-                        <Image src="/logo.png" alt="logo" width={25} height={25} />
+                    <Link className="mr-2 flex items-center space-x-2" href="/dashboard">
+                        <Image src="/ruddy-icon.png" alt="Ruddy logo" width={32} height={32} className="rounded-lg" />
                     </Link>
                     <Suspense fallback={<Badge variant="outline" className="mr-2"><Skeleton className="w-[50px] h-[20px] rounded-full" /></Badge>}>
                         <Badge variant="outline" className="mr-2">{stripePlan}</Badge>
                     </Suspense>
                     <nav className="flex items-center space-x-6 text-sm font-medium">
-                        <Link className="transition-colors hover:text-foreground/80 text-foreground" href="#">
-                            Home
-                        </Link>
-                        <Link className="transition-colors hover:text-foreground/80 text-foreground/60" href="#">
-                            Projects
-                        </Link>
-                        <Link className="transition-colors hover:text-foreground/80 text-foreground/60" href="#">
-                            Tasks
-                        </Link>
-                        <Link className="transition-colors hover:text-foreground/80 text-foreground/60" href="#">
-                            Reports
+                        <Link className="transition-colors hover:text-foreground/80 text-foreground" href="/dashboard">
+                            Overview
                         </Link>
                     </nav>
                 </div>
