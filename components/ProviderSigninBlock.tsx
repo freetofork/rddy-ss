197398d243aefb596fefd2487da1aa1817ad6ae3
Free To Fork <zeroeum@yahoo.com>
2026-03-23 11:22:48 -0300

    Update ProviderSigninBlock.tsx

--- components/ProviderSigninBlock.tsx
+++ components/ProviderSigninBlock.tsx
@@ -1,7 +1,7 @@
 import { FaGoogle, FaGithub } from "react-icons/fa";
 import { signInWithGithub, signInWithGoogle } from '@/app/auth/actions'
 import { Button } from "@/components/ui/button"
-export default function ProviderSigninBlock() {
+export default function ProviderSigninBlock({ plan }: { plan?: string }) {
     const isGoogleEnabled = process.env.GOOGLE_OAUTH_CLIENT_ID ? true : false
     const isGithubEnabled = process.env.GITHUB_OAUTH_CLIENT_ID ? true : false
     return (
@@ -9,6 +9,7 @@ export default function ProviderSigninBlock() {
             <div className="flex flex-row gap-2">
                 {isGoogleEnabled && (
                     <form action={signInWithGoogle} className="basis-full">
+                        <input type="hidden" name="plan" value={plan || ''} />
                         <Button variant="outline" aria-label="Sign in with Google" type="submit" className="w-full">
                             <FaGoogle />
                         </Button>
@@ -16,6 +17,7 @@ export default function ProviderSigninBlock() {
                 )}
                 {isGithubEnabled && (
                     <form action={signInWithGithub} className="basis-full">
+                        <input type="hidden" name="plan" value={plan || ''} />
                         <Button variant="outline" aria-label="Sign in with Github" className="w-full">
                             <FaGithub />
                         </Button>
